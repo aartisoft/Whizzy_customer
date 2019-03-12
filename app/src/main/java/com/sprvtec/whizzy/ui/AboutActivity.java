924aeb5885ca53a37bfe9e6e93af325927872089
Sowjanya <sowjanya14.u@gmail.com>
2019-03-12 15:06:09 +0530

    up

--- app/src/main/java/com/sprvtec/whizzy/ui/AboutActivity.java
+++ app/src/main/java/com/sprvtec/whizzy/ui/AboutActivity.java
@@ -16,7 +16,7 @@ public class AboutActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         findViewById(R.id.back).setOnClickListener(v -> finish());
-//dfdsdgdfgf
+
     }
 
 
